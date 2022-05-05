9019dda6211ce5e562362ea9669e21d92d727d54
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-05-05 21:36:32 +0200

    unit converter completed

--- web-page-internal/page-hierarchy/lessons/Scrimba-3.1/impsi.js
+++ web-page-internal/page-hierarchy/lessons/Scrimba-3.1/impsi.js
@@ -1,37 +1,34 @@
 // sets ratio of Imperial and SI units
-// object {key,[iperialUnit, siUnit]}
-let ratioImpToSi = {};
+// object {unit,[[iperialUnit, ratio], [siUnit, ratio]}
+let ratioImpSi = {};
 // ratios here
 ratioImpSi.length = [
-  // 1 feet to meters
-  0.3048,
-  // 1 meter to feets
-  3.2808398950131233595800524934383];
+  // feet / meter
+  ["feet", 0.3048],
+  // meter / feet
+  ["meter", 3.2808398950131233595800524934383]
+  ];
 
 ratioImpSi.volume = [
-  // 1 Imperial Gallon to liters
-  4.54609,
-  // 1 SI to Imperial
-  0.21996924829908778752730368294512];
+  // imperial gallon / liter
+  ["imperial gallon", 4.54609],
+  // liter / imperial gallon
+  ["liter", 0.21996924829908778752730368294512]
+  ];
 
 ratioImpSi.mass = [
-  // 1 Pound to kilograms
-  0.3048,
-  // 1 SI to Imperial
-  3.2808398950131233595800524934383];
+  // pound / kilogram
+  ["pound", 0.3048],
+  // kilogram / pound
+  ["kilogram", 3.2808398950131233595800524934383]
+  ];
 // end of ratios
 
-// object prototype to hold converted values
-let convertedInput = {};
-for ( key in Object.entries(ratioImpSi)) {
-  convertedInput.`${key}` = ;
-};
-
-// program class
+// program class  
 class converter {
   // Public
   run() {
-    this.getInput();
+    if (!this.getInput() === true) {return}; //input is NAN
     this.convert();
     this.updateOutput();
   };
@@ -39,26 +36,45 @@ class converter {
   // Private
   #currentInput;
   #newInput;
-  // Stores input field in private variable
+
+  // returns false if NAN
   getInput() {
     this.#newInput = document.getElementById("input-field");
-    if (isNaN(this.#newInput)){return}; // throw error? or fail silently?
+    if (isNaN(this.#newInput)){return false}; // throw error? or fail silently?
     this.#currentInput = this.#newInput;
   };
 
-  // converts input number into Imperial and SI units 
+  // converts currentInput to other units
+  // returns {unitType, [unitImperial, outImperial], [unitMetric, outMetric]}
   convertAll() {
+    let convertedInput = {};
+    for (const [type, info] of Object.entries(ratioImpSi)) {
+      convertedInput[type] = [
+          [info[0][0],info[0][1] * currentInput],
+          [info[1][0],info[1][1] * currentInput]
+          ];
+    };
+    
+    // for dev purposes
+    for (const [type, info] of Object.entries(convertedInput)) {
+        console.log("\n" + type);
+        console.log(currentInput + info[1][0] + " = " + info[0][1] + info[0][0]);
+        console.log(currentInput + info[0][0] + " = " + info[1][1] + info[1][0]);
+    };
+    // for dev purposes
 
+    return convertedInput;
   };
-
+  
   // Prints converted number to output fields
   updateOutput() {
 
   };
 };
 
-document.getElementsByClassName()
+// document.getElementsByClassName();
 
 // activate program
 const program = new converter;
+program.run();
 setInterval(program.run(), 500);
