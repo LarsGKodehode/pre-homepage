0ff0b05b58ffabf96482a5d3ab7c472a7ee7d3ae
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-05-13 13:50:13 +0200

    implemented theme switch button

--- web-page-internal/page-hierarchy/lessons/Scrimba-3.4/pwGene.js
+++ web-page-internal/page-hierarchy/lessons/Scrimba-3.4/pwGene.js
@@ -10,8 +10,7 @@ const alphabetStart = 33;
 const alphabetEnd = 127;
 const alphabetSize = alphabetEnd - alphabetStart;
 
-// gets called on button click
-function submit() {
+function generatePassword() {
   for (field in documentOutputFields) {
     let newPassword = "";
     for (i = 0; i < passwordLength; i++) {
@@ -20,3 +19,21 @@ function submit() {
     documentOutputFields[field].value = newPassword;
   };
 };
+
+function switchTheme() {
+  console.log("button clicked");
+  let currentTheme = document.body.dataset.theme;
+  switch (currentTheme) {
+    case "dark":
+      document.body.dataset.theme = "light";
+      console.log("new theme is light")
+      break;
+    case "light":
+      document.body.dataset.theme = "dark";
+      console.log("new theme is dark")
+      break;
+  };
+};
+
+const themeSwitch = document.getElementById("theme-switcher");
+themeSwitch.addEventListener("click", switchTheme);
