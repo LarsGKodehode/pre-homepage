59229a54b8166de49c88fb8449336de68909a78b
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-05-15 16:09:30 +0200

    minor format adjustments

--- web-page-internal/page-hierarchy/lessons/Scrimba-3.4/pwGene.js
+++ web-page-internal/page-hierarchy/lessons/Scrimba-3.4/pwGene.js
@@ -4,8 +4,7 @@ const documentOutputFields = document.getElementsByClassName("output-field");
 
 // how many symbols in password
 const passwordLength = 16;
-
-// this is a sensible selection of utf-8 charecters
+// sensible selection of utf-8 charecters
 const alphabetStart = 33;
 const alphabetEnd = 127;
 const alphabetSize = alphabetEnd - alphabetStart;
@@ -20,10 +19,9 @@ function generatePassword() {
   };
 };
 
-
 // theme switcher
 function switchTheme() {
-  let currentTheme = document.body.dataset.theme;
+  const currentTheme = document.body.dataset.theme;
   switch (currentTheme) {
     case "dark":
       document.body.dataset.theme = "light";
@@ -33,6 +31,6 @@ function switchTheme() {
       break;
   };
 };
-// setup listener
+
 const themeSwitch = document.getElementById("theme-switcher");
 themeSwitch.addEventListener("click", switchTheme);
