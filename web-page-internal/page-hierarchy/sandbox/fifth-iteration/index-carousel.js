1754450ac4abeecbb5547e99190609ddf0208c0a
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-06-30 16:41:15 +0200

    some morew initial work

--- web-page-internal/page-hierarchy/sandbox/fifth-iteration/index-carousel.js
+++ web-page-internal/page-hierarchy/sandbox/fifth-iteration/index-carousel.js
@@ -4,20 +4,28 @@ And to try to be more in line with what I get is a trending Javascript conventio
 */
 /*
 NOTE TO SELF: Scoping hackery 101
-Self contained modules works by creating variables and then discarding all references to them
-except those in the returned members/methods. Making them inaccessible to everything except those.
+Self contained modules works by creating variables and then discarding all references to them,
+except those in the returned members/methods. Making them inaccessible to everything but those.
 Works because of Garbage Collection cleaning up all your dirty things.
  */
 
 /**
- * 
- * @returns DOM Node
- */
-const Carousel = () => {
+     * Fetches the next or previous item from items
+     *
+     * @param container {JQueryElement} scroll-container in which the items can be found
+     * @param items {Array} items to be searched through
+     * @param isNext {boolean} set to true (default) if you want the next item, to false if you want the previous one
+     * @returns {*}
+     */
+const Carousel = (container, items, isNext) => {
 
-const getNode = () => {};
+  const carousel = {
 
-const addProject = () => {};
+  };
+
+  const getNode = () => {};
+
+  const addProject = () => {};
 
 
   // public members/methods returned in this object
@@ -26,4 +34,4 @@ const addProject = () => {};
     addProject,
   };
 };
-export const carousel = Carousel();
+export default Carousel;
