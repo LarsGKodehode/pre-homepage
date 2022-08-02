ff1ebd78da77869d8748fd61c4b30ec493a8f8fd
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-08-02 14:11:02 +0200

    minor fix

--- web-page-internal/page-hierarchy/sandbox/fifth-iteration/index-carousel.js
+++ web-page-internal/page-hierarchy/sandbox/fifth-iteration/index-carousel.js
@@ -1,37 +1,37 @@
 /*
-Using the Module Design Pattern to imitate private members and methods.
+Using the Module Design Pattern to imitate classes with private members and methods.
 And to try to be more in line with what I get is a trending Javascript convention.
 */
 /*
 NOTE TO SELF: Scoping hackery 101
 Self contained modules works by creating variables and then discarding all references to them,
-except those in the returned members/methods. Making them inaccessible to everything but those.
+except those in the returned members/methods. Making them inaccessible to everything but them.
 Works because of Garbage Collection cleaning up all your dirty things.
  */
 
 /**
-     * Fetches the next or previous item from items
-     *
-     * @param container {JQueryElement} scroll-container in which the items can be found
-     * @param items {Array} items to be searched through
-     * @param isNext {boolean} set to true (default) if you want the next item, to false if you want the previous one
+     * Create a carusel virtual DOM node as specified
+     * @param createInfo {Object} object containing all the variables used for initialization of "psuedo class"
      * @returns {*}
      */
-const Carousel = (container, items, isNext) => {
+const Carousel = (createInfo) => {
 
-  const carousel = {
+  const carousel = {}; // virtual carousel object
 
-  };
-
-  const getNode = () => {};
+  const getCarousel = () => {};
 
   const addProject = () => {};
 
 
-  // public members/methods returned in this object
+  // public members/methods to expose
   return {
-    getNode,
+    getCarousel,
     addProject,
   };
 };
-export default Carousel;
+
+// This works
+export const carusel = Carousel();
+
+// This does not
+// export default Carousel();
